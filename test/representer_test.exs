d373dcd0067df0d5bf65a7c6b663ba4e93698be4
Tim Austin <tim@neenjaw.com>
2020-09-04 00:38:09 -0600

    create dir on test

--- test/representer_test.exs
+++ test/representer_test.exs
@@ -2,6 +2,8 @@ defmodule RepresenterTest do
   use ExUnit.Case
 
   test "run" do
+    File.mkdir!("./test_output")
+
     Representer.process(
       "./test_data/hello_world.ex",
       "./test_output/representation.txt",
