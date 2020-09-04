156bc7900b2df771c4e623d1219be4c6d34da944
Tim Austin <tim@neenjaw.com>
2020-09-04 00:07:58 -0600

    change name

--- bin/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir="$(dirname "${0}")"
-slug="${1}"
-solution_dir="${2}"
-
-printf "representing exercise %q in %q\n" "${slug}" "${solution_dir}"
-
-"${dir}/elixir_representer" "${slug}" "${solution_dir}"
