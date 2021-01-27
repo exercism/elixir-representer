b1a51a3c69f4424be9085ca1d56b4bbc8a6b3809
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-01-27 09:42:21 +0100

    Try to make the representer work on v3 staging website (#15)

--- test/representer_test.exs
+++ test/representer_test.exs
@@ -2,12 +2,18 @@ defmodule RepresenterTest do
   use ExUnit.Case
 
   test "run" do
-    File.mkdir!("./test_output")
+    File.mkdir_p!("./test_output")
 
     Representer.process(
       "./test_data/hello_world.ex",
       "./test_output/representation.txt",
       "./test_output/mapping.json"
     )
+
+    assert File.read!("./test_output/representation.txt") ==
+             File.read!("./test_data/expected_representation.txt")
+
+    assert File.read!("./test_output/mapping.json") ==
+             File.read!("./test_data/expected_mapping.json")
   end
 end
