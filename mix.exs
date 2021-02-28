8f2e573da189a545307d2b6718759d7a69adf976
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-02-28 20:33:19 +0100

    Run on Elixir 1.11 (#17)
    Co-authored-by: Tim Austin <tim@neenjaw.com>

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Representer.MixProject do
     [
       app: :representer,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: escript()
