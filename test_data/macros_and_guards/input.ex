03f6bece3a19714080142cafe98accc200a79542
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-08-07 18:28:37 +0200

    Add test for custom guard usage

--- test_data/macros_and_guards/input.ex
+++ test_data/macros_and_guards/input.ex
@@ -5,4 +5,8 @@ defmodule User do
   defmacrop do_validate_user, do: true
   defguard is_even(term) when is_integer(term) and rem(term, 2) == 0
   defguardp is_odd(term) when is_integer(term) and rem(term, 2) == 1
+
+  def even?(term) when is_even(term), do: true
+  def even?(term), do: false
+  def odd?(term), do: is_odd(term)
 end
