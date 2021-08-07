c6d66c2b9a18979f5f262555ca305ae1447afc20
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-08-07 16:29:21 +0200

    Make it possible to run single test

--- test/representer_test.exs
+++ test/representer_test.exs
@@ -6,25 +6,30 @@ defmodule RepresenterTest do
   end
 
   describe "test_data" do
-    for directory <- File.ls!("test_data") do
-      test "#{directory}" do
-        dir = unquote(directory)
-        output_directory = Path.join(["./test_output", dir])
-        File.mkdir_p!(output_directory)
-
-        input = Path.join(["./test_data", dir, "input.ex"])
-
-        expected_representation = Path.join(["./test_data", dir, "expected_representation.txt"])
-        expected_mapping = Path.join(["./test_data", dir, "expected_mapping.json"])
-
-        output_representation = Path.join(["./test_output", dir, "representation.txt"])
-        output_mapping = Path.join(["./test_output", dir, "mapping.json"])
-
-        Representer.process(input, output_representation, output_mapping)
-
-        assert File.read!(output_representation) == File.read!(expected_representation)
-        assert File.read!(output_mapping) == File.read!(expected_mapping)
-      end
+    test "simple_single_module" do
+      test_directory("simple_single_module")
     end
+
+    test "two_modules" do
+      test_directory("two_modules")
+    end
+  end
+
+  defp test_directory(dir) do
+    output_directory = Path.join(["./test_output", dir])
+    File.mkdir_p!(output_directory)
+
+    input = Path.join(["./test_data", dir, "input.ex"])
+
+    expected_representation = Path.join(["./test_data", dir, "expected_representation.txt"])
+    expected_mapping = Path.join(["./test_data", dir, "expected_mapping.json"])
+
+    output_representation = Path.join(["./test_output", dir, "representation.txt"])
+    output_mapping = Path.join(["./test_output", dir, "mapping.json"])
+
+    Representer.process(input, output_representation, output_mapping)
+
+    assert File.read!(output_representation) == File.read!(expected_representation)
+    assert File.read!(output_mapping) == File.read!(expected_mapping)
   end
 end
