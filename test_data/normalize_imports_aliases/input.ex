967e3517e72a543334522a21254b0990c7dcd489
Jeremie Gillet <jie.gillet@gmail.com>
2022-09-12 16:16:29 +0900

    Remove stowaway file

--- test_data/normalize_imports_aliases/input.ex
+++
@@ -1,8 +0,0 @@
-defmodule A do
-  import B, only: [b_function: 1]
-  import C, except: [c_function: 1]
-  alias D, as: Dog
-  def use_b(x), do: x |> b_function
-  def use_c(c_function), do: c_function(0)
-  def use_d(y), do: Dog.sniff(y)
-end
